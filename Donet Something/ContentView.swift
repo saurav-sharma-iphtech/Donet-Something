c98ecc0f9ecbe5fab11e2663e9beb301a0ff67fa
saurav-sharma-iphtech <saurav.sharma@iphtechnologies.com>
2025-05-28 18:04:28 +0530

    Initial Commit

---
+++ Donet Something/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Donet Something
+//
+//  Created by iPHTech 26 on 28/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Donet Something/Donet_SomethingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Donet_SomethingApp.swift
+//  Donet Something
+//
+//  Created by iPHTech 26 on 28/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Donet_SomethingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
